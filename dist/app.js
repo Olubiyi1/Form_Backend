26dc0321258a7f6217a0579888c28c2263b6b517
olubiyi babajide <olubiyibabajide@gmail.com>
2025-04-04 23:34:46 +0100

    solving render issues

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
     origin: "*",
-    methods: ["GET", "PUT,", "POST", "DELETE"],
+    methods: ["GET", "PUT", "POST", "DELETE"],
     allowedHeaders: ["content-type", "Authorization"]
 }));
 // allowing express access json files
